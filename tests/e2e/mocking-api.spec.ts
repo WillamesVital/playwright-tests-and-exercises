685ca51ab8b104244a753555450e0b6d46516726
WillamesVital <willamescfvital@gmail.com>
2024-10-31 21:49:44 -0300

    Adicionar testes de clock e mocking apis

---
+++ tests/e2e/mocking-api.spec.ts
@@ -0,0 +1,62 @@
+import { test, expect, type Page } from '@playwright/test';
+
+// https://playwright.dev/docs/mock#mock-api-requests
+
+test.describe('Mocking an API call', () => {
+
+  test('mocks a fruit and does not call api', async ({ page }) => {
+    // Mock the api call before navigating
+    await page.route('*/**/api/v1/fruits', async (route) => {
+      const json = [{ name: 'Strawberry', id: 21 }];
+      await route.fulfill({ json });
+      console.log(json);
+    });
+    // Go to the page
+    await page.goto('https://demo.playwright.dev/api-mocking');
+  
+    // Assert that the Strawberry fruit is visible
+    await expect(page.getByText('Strawberry')).toBeVisible();
+  });
+  
+});
+
+test.describe('Intercepting the request and modifying it', () => {
+
+  test('gets the json from api and adds a new fruit', async ({ page }) => {
+    // Get the response and add to it
+    await page.route('*/**/api/v1/fruits', async (route) => {
+      const response = await route.fetch();
+      const json = await response.json();
+      json.push({ name: 'Playwright', id: 100 });
+      // Fulfill using the original response, while patching the response body
+      // with the given JSON object.
+      await route.fulfill({ response, json });
+      console.log(json);
+    });
+
+    // Go to the page
+    await page.goto('https://demo.playwright.dev/api-mocking');
+
+    // Assert that the new fruit is visible
+    await expect(page.getByText('Playwright', { exact: true })).toBeVisible();
+  });
+  
+});
+
+test.describe('Mocking with HAR files', () => {
+
+  test('records or updates the HAR file', async ({ page }) => {
+    // Get the response from the HAR file
+    await page.routeFromHAR('./hars/fruits.har', {
+      url: '*/**/api/v1/fruits',
+      update: true,
+    });
+
+    // Go to the page
+    await page.goto('https://demo.playwright.dev/api-mocking');
+
+    // Assert that the Playwright fruit is visible
+    await expect(page.getByText('Strawberry')).toBeVisible();
+  });
+  
+});
